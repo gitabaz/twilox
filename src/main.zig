cdf9335d9cfa96dacd3256fce46faeb394b44440
Aidan Zabalo <olabaz@gmail.com>
2024-10-05 11:50:19 -0400

    Use relative path instead of absolute path for running file

--- src/main.zig
+++ src/main.zig
@@ -9,7 +9,7 @@ pub fn main() !void {
     defer std.process.argsFree(allocator, args);
 
     if (args.len > 2) {
-        std.debug.print("Usage: twilox [script]", .{});
+        std.debug.print("Usage: twilox [script]\n", .{});
     } else if (args.len == 2) {
         try runFile(allocator, args[1]);
     } else {
@@ -18,11 +18,13 @@ pub fn main() !void {
 }
 
 fn runFile(allocator: std.mem.Allocator, path: []const u8) !void {
-    std.debug.print("{s}\n", .{path});
-    const file = try std.fs.openFileAbsolute(path, .{ .mode = .read_only });
+    const file = try std.fs.cwd().openFile(path, .{ .mode = .read_only });
+    defer file.close();
+
     const contents = try file.readToEndAlloc(allocator, 4096);
-    std.debug.print("{s}\n", .{contents});
     defer allocator.free(contents);
+
+    std.debug.print("{s}\n", .{contents});
 }
 
 fn runPrompt() void {
