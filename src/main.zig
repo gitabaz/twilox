739640d77358dae9dd70d77d89f061a999468a07
Aidan Zabalo <olabaz@gmail.com>
2024-10-05 21:07:01 -0400

    Refactor run functions into Lox struct

--- src/main.zig
+++ src/main.zig
@@ -8,25 +8,61 @@ pub fn main() !void {
     const args = try std.process.argsAlloc(allocator);
     defer std.process.argsFree(allocator, args);
 
+    var lox: Lox = .{};
+
     if (args.len > 2) {
         std.debug.print("Usage: twilox [script]\n", .{});
     } else if (args.len == 2) {
-        try runFile(allocator, args[1]);
+        try lox.runFile(allocator, args[1]);
     } else {
-        runPrompt();
+        try lox.runPrompt(allocator);
     }
 }
 
-fn runFile(allocator: std.mem.Allocator, path: []const u8) !void {
-    const file = try std.fs.cwd().openFile(path, .{ .mode = .read_only });
-    defer file.close();
+pub const Lox = struct {
+    has_error: bool = false,
 
-    const contents = try file.readToEndAlloc(allocator, 4096);
-    defer allocator.free(contents);
+    const Self = @This();
 
-    std.debug.print("{s}\n", .{contents});
-}
+    fn runFile(self: *Self, allocator: std.mem.Allocator, path: []const u8) !void {
+        const file = try std.fs.cwd().openFile(path, .{ .mode = .read_only });
+        defer file.close();
 
-fn runPrompt() void {
-    std.debug.print("{s}\n", .{"prompt"});
-}
+        const contents = try file.readToEndAlloc(allocator, 4096);
+        defer allocator.free(contents);
+
+        run(contents);
+
+        if (self.has_error) {
+            return error.FileError;
+        }
+    }
+
+    fn run(contents: []const u8) void {
+        std.debug.print("{s}\n", .{contents});
+    }
+
+    fn runPrompt(self: *Self, allocator: std.mem.Allocator) !void {
+        const stdin = std.io.getStdIn().reader();
+        const stdout = std.io.getStdOut().writer();
+
+        while (true) {
+            try stdout.print("# ", .{});
+            const line = try stdin.readUntilDelimiterOrEofAlloc(allocator, '\n', 4096);
+            if (line) |contents| {
+                defer allocator.free(contents);
+                run(contents);
+                self.has_error = false;
+            } else {
+                break;
+            }
+        }
+    }
+
+    fn report(self: *Self, line: u8, where: []const u8, message: []const u8) !void {
+        const stderr = std.io.getStdErr().writer();
+        try stderr.print("[line {d}] Error{s}: {s}", .{ line, where, message });
+
+        self.has_error = true;
+    }
+};
