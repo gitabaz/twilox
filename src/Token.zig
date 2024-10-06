69e9cbf36402f0de60f335555407fdf0f0932a37
Aidan Zabalo <olabaz@gmail.com>
2024-10-05 21:23:39 -0400

    Add tokens

---
+++ src/Token.zig
@@ -0,0 +1,94 @@
+const std = @import("std");
+
+pub const Token = struct {
+    type: TokenType,
+    lexeme: []const u8,
+    literal: ?Literal,
+    line: u8,
+
+    const Self = @This();
+
+    pub fn toString(self: *Self, allocator: std.mem.Allocator) ![]const u8 {
+        const str = try std.fmt.allocPrint(allocator, "{s} {s} {?}", .{
+            @tagName(self.type),
+            self.lexeme,
+            self.literal,
+        });
+
+        return str;
+    }
+};
+
+pub const Literal = union {
+    str: []const u8,
+    float: f64,
+};
+
+pub const Keywords = std.StaticStringMap(TokenType).initComptime(.{
+    .{ "and", .AND },
+    .{ "class", .CLASS },
+    .{ "else", .ELSE },
+    .{ "false", .FALSE },
+    .{ "for", .FOR },
+    .{ "fun", .FUN },
+    .{ "if", .IF },
+    .{ "nil", .NIL },
+    .{ "or", .OR },
+    .{ "print", .PRINT },
+    .{ "return", .RETURN },
+    .{ "super", .SUPER },
+    .{ "this", .THIS },
+    .{ "true", .TRUE },
+    .{ "var", .VAR },
+    .{ "while", .WHILE },
+});
+
+pub const TokenType = enum {
+    // Single-character tokens.
+    LEFT_PAREN,
+    RIGHT_PAREN,
+    LEFT_BRACE,
+    RIGHT_BRACE,
+    COMMA,
+    DOT,
+    MINUS,
+    PLUS,
+    SEMICOLON,
+    SLASH,
+    STAR,
+
+    // One or two character tokens.
+    BANG,
+    BANG_EQUAL,
+    EQUAL,
+    EQUAL_EQUAL,
+    GREATER,
+    GREATER_EQUAL,
+    LESS,
+    LESS_EQUAL,
+
+    // Literals.
+    IDENTIFIER,
+    STRING,
+    NUMBER,
+
+    // Keywords.
+    AND,
+    CLASS,
+    ELSE,
+    FALSE,
+    FUN,
+    FOR,
+    IF,
+    NIL,
+    OR,
+    PRINT,
+    RETURN,
+    SUPER,
+    THIS,
+    TRUE,
+    VAR,
+    WHILE,
+
+    EOF,
+};
